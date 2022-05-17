e34c59a773939ddc9e34b03981b8185f14862ce2
Anton Sorokin <ams2001@mail.ru>
2022-05-18 00:28:51 +0300

    added main function for profiling via `stack build && time stack run`

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,9 @@
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Main where
-import QLinear
-import Topoll.ChainComplex.Type
-import Topoll.ChainComplex.Field
-import Data.Ratio (Ratio)
+import Topoll.SimplicialSet
+import Data.Set (delete, powerSet, fromList)
 
 main :: IO ()
 main = do
-  putStrLn $ reprChainComplexModules simpleComplex
-  print $ bettiNumbers simpleComplex
-
-simpleComplex :: ChainComplex (Ratio Integer) '[0, 5, 2, 2, 0]
-Just simpleComplex =
-      startComplex
-      <<<
-      [matrix|0 0;
-              0 1|]
-      <<<
-      [matrix|2 0 0 0 0;
-              0 0 0 0 0|]
-      <<<
-      zeroMatrix
+  print $ simplicialHomologyOverQ $ simplicialSet (fromList [1..10] `delete` powerSet (fromList [1..10]))
