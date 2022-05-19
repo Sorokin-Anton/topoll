9d2f9d8be830f81c6ff2a475c7fd3c78169fc477
Gleb Krasilich <gleb.chili@gmail.com>
2022-05-19 23:13:16 +0300

    chooseRandomLandmars implemented

--- src/Topoll/DistanceMatrix/DistanceMatrix.hs
+++ src/Topoll/DistanceMatrix/DistanceMatrix.hs
@@ -1,4 +1,4 @@
-module Topoll.DistanceMatrix.DistanceMatrix (AggregatedData(..), chooseMaxminLandmakrs) where
+module Topoll.DistanceMatrix.DistanceMatrix (AggregatedData(..), chooseMaxminLandmakrs, chooseRandomLandmarks) where
 
 import qualified Data.Vector as V
 import Data.Vector (Vector, (!))
@@ -22,9 +22,9 @@ distToVectors vec points = points <&> (\v -> euclideanMetric v vec)
 chooseMaxminLandmarksIter :: Natural -> AggregatedData -> IO AggregatedData
 chooseMaxminLandmarksIter 0 points = return points
 chooseMaxminLandmarksIter 1 (AggregatedData _ dpoints) = do
-    let numberOfDpoints = V.length dpoints
+    let numberOfDataPoints = V.length dpoints
     gen <- newStdGen
-    let (rndPos, _) = uniformR (0 :: Int, numberOfDpoints - 1) gen
+    let (rndPos, _) = uniformR (0, numberOfDataPoints - 1) gen
     let rndDpoint = dpoints ! rndPos
     let initDpoints = V.slice 0 rndPos dpoints
     let tailDpoints = V.drop (rndPos + 1) dpoints
@@ -43,10 +43,27 @@ chooseMaxminLandmakrs :: Natural -> Vector (Vector Float) -> IO AggregatedData
 chooseMaxminLandmakrs numberOfLandmarksToChoose samplePoints = 
     chooseMaxminLandmarksIter numberOfLandmarksToChoose (AggregatedData V.empty samplePoints)
 
+chooseRandomLandmarksIter :: Natural -> AggregatedData -> IO AggregatedData
+chooseRandomLandmarksIter 0 dta = return dta
+chooseRandomLandmarksIter n dta = do
+    AggregatedData landmrks dpoints <- chooseMaxminLandmarksIter (n - 1) dta
+    gen <- newStdGen
+    let numberOfDataPoints = V.length dpoints
+    let (rndPos, _) = uniformR (0, numberOfDataPoints - 1) gen
+    let newLandmark = dpoints ! rndPos
+    let initDpoints = V.slice 0 rndPos dpoints
+    let tailDpoints = V.drop (rndPos + 1) dpoints
+    return (AggregatedData (landmrks <> V.singleton newLandmark) (initDpoints <> tailDpoints))
+
+{- Choose landmark points from the sample uniformly at random -}
+chooseRandomLandmarks :: Natural -> Vector (Vector Float) -> IO AggregatedData
+chooseRandomLandmarks numberOfLandmarksToChoose samplePoints = 
+    chooseRandomLandmarksIter numberOfLandmarksToChoose (AggregatedData V.empty samplePoints)
+
 vecHelper :: [[Float]] -> Vector (Vector Float)
 vecHelper lst = V.fromList (fmap V.fromList lst)
 
 {-
->>> unsafePerformIO $ chooseMaxminLandmakrs 1 (vecHelper [[1,2], [3,4], [5, 6], [7, 8], [9, 10], [11, 12]])
-AggregatedData {landmarkPoints = [[9.0,10.0]], dataPoints = [[1.0,2.0],[3.0,4.0],[5.0,6.0],[7.0,8.0],[11.0,12.0]]}
+>>> unsafePerformIO $ chooseRandomLandmarks 4 (vecHelper [[1,2], [3,4], [5, 6], [7, 8], [9, 10], [11, 12]])
+AggregatedData {landmarkPoints = [[7.0,8.0],[1.0,2.0],[11.0,12.0],[5.0,6.0]], dataPoints = [[3.0,4.0],[9.0,10.0]]}
 -}
