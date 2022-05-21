9f3888c5a72bd8246f3860580479adea890e94f0
Gleb Krasilich <gleb.chili@gmail.com>
2022-05-21 23:25:10 +0300

    sampleSpherePointsUniformlyAtParametrization now can sample points from the spheres of different radius

--- src/Topoll/Samplers/Samplers.hs
+++ src/Topoll/Samplers/Samplers.hs
@@ -14,19 +14,21 @@ nextTriple firstRange secondRange (_, _, rnd) = (firstC, fst nextPair, snd nextP
     (firstC, rnd') = uniformR firstRange rnd
     nextPair = uniformR secondRange rnd'
 
-sampleSpherePointsUniformlyAtParametrization :: Int -> IO (Vector (Vector Float))
-sampleSpherePointsUniformlyAtParametrization ((<0) -> True) = fail "The sample length can't be negative"
-sampleSpherePointsUniformlyAtParametrization 0 = return V.empty
-sampleSpherePointsUniformlyAtParametrization numberOfPointsToSample = do
+sampleSpherePointsUniformlyAtParametrization :: Float -> Int -> IO (Vector (Vector Float))
+sampleSpherePointsUniformlyAtParametrization _ ((<0) -> True) = fail "The sample length can't be negative"
+sampleSpherePointsUniformlyAtParametrization ((<0) -> True) _ = fail "Can't sample points from the sphere of the negative radius"
+sampleSpherePointsUniformlyAtParametrization _ 0 = return V.empty
+sampleSpherePointsUniformlyAtParametrization sphereRadius numberOfPointsToSample = do
     rndGen <- newStdGen
     let (frst, rndGen') = uniformR (0 :: Float, 2 * pi) rndGen
     let (scnd, rndGen'') = uniformR (0 :: Float, 2 * pi) rndGen'
     let firstTriple = (frst, scnd, rndGen'')
     let preResult = V.iterateN numberOfPointsToSample (nextTriple (0 :: Float, 2 * pi) (0 :: Float, 2 * pi)) firstTriple
     let preResult' = preResult <&> (\(ft, nd, _) -> (ft, nd))
-    return $ preResult' <&> (\(x, y) -> V.fromList [cos x * sin y, sin x * sin y, cos y])
+    return $ preResult' <&> (\(x, y) -> 
+        V.fromList [sphereRadius * cos x * sin y, sphereRadius * sin x * sin y, sphereRadius * cos y])
 
 {-
->>> unsafePerformIO (sampleSpherePointsUniformlyAtParametrization 2)
-[[-0.4252499,-8.684096e-2,0.9009002],[-0.6159266,-0.7711275,0.16123512]]
+>>> unsafePerformIO (sampleSpherePointsUniformlyAtParametrization (-3) 2)
+user error (Can't sample points from the sphere of the negative radius)
 -}
